b6e1c13d729a8c5a750579469adf1b29b12d08f2
TSUYUSATO Kitsune <make.just.on@gmail.com>
2016-02-28 16:11:39 +0900

    Update Main.java to use variable-length arguments

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ import java.util.regex.*;
 public class Main {
   static Scanner scan = new Scanner(System.in);
 
-  public static void main(String[] args) {
+  public static void main(String... args) {
     scan.useDelimiter("\\s*(\\n|,)\\s*");
   }
 }
