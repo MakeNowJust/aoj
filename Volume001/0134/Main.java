2640faadb54ef76c265e1811a4e0a61314bf5db2
TSUYUSATO Kitsune <make.just.on@gmail.com>
2016-03-02 11:20:04 +0900

    Accepted '0134: Exit Survey'

---
+++ Volume001/0134/Main.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.*;
+import java.time.*;
+import java.time.temporal.*;
+import java.util.*;
+import java.util.function.*;
+import java.util.regex.*;
+
+public class Main {
+  static Scanner scan = new Scanner(System.in);
+
+  public static void main(String... args) {
+    int n = scan.nextInt();
+    double mean = 0;
+    for (int i = 0; i < n; i++) {
+      mean += scan.nextDouble() / n;
+    }
+    System.out.println((int)mean);
+  }
+}
