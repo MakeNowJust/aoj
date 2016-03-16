50129ee60742f154cdee0670ac6bd6ab265e9bbe
TSUYUSATO Kitsune <make.just.on@gmail.com>
2016-03-16 17:52:00 +0900

    Accepted '0151: Grid'

---
+++ Volume001/0151/Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.math.*;
+import java.time.*;
+import java.time.temporal.*;
+import java.util.*;
+import java.util.function.*;
+import java.util.regex.*;
+
+public class Main {
+  static Scanner scan = new Scanner(System.in);
+
+  public static void main(String... args) {
+    scan.useDelimiter("\\s*(\\n|,)\\s*");
+  }
+}
