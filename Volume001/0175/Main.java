d3b63f846939d07284f8d7e19da25184f3c2adcc
TSUYUSATO Kitsune <make.just.on@gmail.com>
2016-05-13 00:38:55 +0900

    Accepted '0175: A King in Hawaii'

---
+++ Volume001/0175/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.math.*;
+import java.time.*;
+import java.time.temporal.*;
+import java.util.*;
+import java.util.function.*;
+import java.util.regex.*;
+
+public class Main {
+  static Scanner scan = new Scanner(System.in);
+
+  public static void main(String... args) {
+    for (;;) {
+      int i = scan.nextInt();
+      if (i == -1) break;
+      System.out.println(Integer.toString(i, 4));
+    }
+  }
+}
